ccb62321c1aa8328fceaa926517997293673dd0e
Till Wegmüller <toasterson@gmail.com>
2017-01-22 17:25:29 +0100

    Refactor Token Getting into own Function

--- main.go
+++ main.go
@@ -9,7 +9,9 @@ import (
 	"log"
 )
 
-func main() {
+var json_token_file = "token.json"
+
+func NewAuthToken(){
 	config := &oauth2.Config{
 		ClientID: "369013585757-guuiqrkr32k2leb4kd2tspfff83g7dic.apps.googleusercontent.com",
 		ClientSecret: "-TrVOp1NCd2a4voHv2BOLBAh",
@@ -22,7 +24,7 @@ func main() {
 	}
 	ctx := context.Background()
 	url := config.AuthCodeURL("state", oauth2.AccessTypeOffline)
-	fmt.Printf("Visit the URL for the auth dialog: %v", url)
+	fmt.Printf("Visit the URL for the auth dialog: %v\n", url)
 	var code string
 	if _, err := fmt.Scan(&code); err != nil {
 		log.Fatal(err)
@@ -32,14 +34,22 @@ func main() {
 		log.Fatal(err)
 	}
 
-	authStorage := &contacts.FileAuthStorage{"/tmp/token.json"}
+	authStorage := &contacts.FileAuthStorage{json_token_file}
 	authDetails := contacts.AuthDetails{
 		AccessToken: tok.AccessToken,
 		RefreshToken: tok.RefreshToken,
 	}
 	authStorage.Save(&authDetails)
+}
+
+
+
+func main() {
+	if _, err := os.Stat(json_token_file); os.IsNotExist(err){
+		NewAuthToken()
+	}
 	apiclient := contacts.NewClient(&contacts.StandardAuthManager{
-		AuthStorage: authStorage,
+		AuthStorage: &contacts.FileAuthStorage{json_token_file},
 		AccessTokenRetriever: &contacts.StandardAccessTokenRetriever{
 			ClientID: "",
 			GoogleSecret: "",
